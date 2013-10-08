b98f84816e7bceb730dbe2825d5a82b46d218c34
Mathis Dirksen-Thedens <dev@zephyrsoft.org>
2013-10-08 15:14:48 +0000

    corrected reference to user.home
    git-svn-id: file:///home/svnroot-zephyrsoft/projects/jmultiburn-sermon/trunk@428 f37aa4ac-6963-46a2-a1ff-558a307fa857

--- src/main/java/org/zephyrsoft/jmultiburn/sermon/PropertyHolder.java
+++ src/main/java/org/zephyrsoft/jmultiburn/sermon/PropertyHolder.java
@@ -6,23 +6,30 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 
 public class PropertyHolder {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(PropertyHolder.class);
+	
 	@Autowired
 	@Qualifier("defaultProperties")
 	private Properties properties;
 	
-	@Value("${user.home}/.jmultiburn-sermon/application.properties")
+	@Value("#{systemProperties['user.home']}/.jmultiburn-sermon/application.properties")
 	private String userPropertiesPath;
 	
 	public void init() throws IOException {
 		File userPropertiesFile = new File(userPropertiesPath);
 		if (userPropertiesFile.exists() && userPropertiesFile.canRead()) {
+			LOG.info("loading user settings");
 			properties.load(new FileReader(userPropertiesFile));
+		} else {
+			LOG.info("no user settings found");
 		}
 	}
 	
